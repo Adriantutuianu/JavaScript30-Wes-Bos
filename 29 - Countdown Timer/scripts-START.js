8f4ffe061b36603e567125b256bfe882a20c6db4
Adrian <adriantutuianu01@gmail.com>
2021-10-14 22:53:05 +0100

    Stop timer at 0

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,9 +1,18 @@
+let countdown;
+
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  setInterval(() => {
-    const secondsLeft = (then - Date.now()) / 1000;
 
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    //check if we should stop it
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      return;
+    }
+    //display it
     console.log(secondsLeft);
   }, 1000);
 }
