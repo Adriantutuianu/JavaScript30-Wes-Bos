cf22add95eb458c07e102c2c1be5e37194717725
Adrian <adriantutuianu01@gmail.com>
2021-10-14 23:06:47 +0100

    HTML title

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -25,5 +25,6 @@ function displayTimeLeft(seconds) {
   const display = `${minutes}:${
     remainderSeconds < 10 ? "0" : ""
   }${remainderSeconds}`;
+  document.title = display;
   timerDisplay.textContent = display;
 }
